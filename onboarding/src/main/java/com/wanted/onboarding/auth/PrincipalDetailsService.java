da6076150f94195b1ad408efccf3b8b20143d3df
Songdoeon <ehdjs9583@naver.com>
2023-08-13 12:03:20 +0900

    principalDetails

---
+++ onboarding/src/main/java/com/wanted/onboarding/auth/PrincipalDetailsService.java
@@ -0,0 +1,23 @@
+package com.wanted.onboarding.auth;
+
+import com.wanted.onboarding.model.User;
+import com.wanted.onboarding.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+
+		return new PrincipalDetails(user);
+	}
+}
