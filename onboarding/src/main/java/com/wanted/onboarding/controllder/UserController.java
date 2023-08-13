2e9e567c4b38d19490647ad59f0037f5f7d1be47
Songdoeon <ehdjs9583@naver.com>
2023-08-13 12:07:59 +0900

    userController

--- onboarding/src/main/java/com/wanted/onboarding/controllder/UserController.java
+++ onboarding/src/main/java/com/wanted/onboarding/controllder/UserController.java
@@ -1,19 +1,15 @@
 package com.wanted.onboarding.controllder;
 
 import com.wanted.onboarding.dto.UserRequest;
-import com.wanted.onboarding.model.User;
-import com.wanted.onboarding.repository.UserRepository;
 import com.wanted.onboarding.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class RestApiController {
+public class UserController {
 
     private final UserService userService;
 
