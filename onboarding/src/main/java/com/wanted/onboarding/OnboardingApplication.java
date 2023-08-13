f5b8f50879c5bd21b084d3f090222d0b9feff9e5
Songdoeon <ehdjs9583@naver.com>
2023-08-13 12:04:30 +0900

    Auditing Annotation

--- onboarding/src/main/java/com/wanted/onboarding/OnboardingApplication.java
+++ onboarding/src/main/java/com/wanted/onboarding/OnboardingApplication.java
@@ -2,8 +2,10 @@ package com.wanted.onboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OnboardingApplication {
 
 	public static void main(String[] args) {
