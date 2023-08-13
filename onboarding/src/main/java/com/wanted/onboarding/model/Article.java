78e4e326b09d79c3672a20958514d415dca81896
Songdoeon <ehdjs9583@naver.com>
2023-08-13 12:01:38 +0900

    entity

---
+++ onboarding/src/main/java/com/wanted/onboarding/model/Article.java
@@ -0,0 +1,32 @@
+package com.wanted.onboarding.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "article")
+public class Article extends BaseTime{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String title;
+
+    @Column(length = 500, nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private User writer;
+}
---
+++ onboarding/src/main/java/com/wanted/onboarding/model/User.java
@@ -0,0 +1,34 @@
+package com.wanted.onboarding.model;
+import javax.persistence.*;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    private String roles;
+
+    public List<String> getRoleList(){
+        if(this.roles.length() > 0){
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+}
