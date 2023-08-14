2111674596948dad6ceaf577dc05fb6a12d1c5fd
Songdoeon <ehdjs9583@naver.com>
2023-08-14 16:37:34 +0900

    jwt token 설정값 변경

--- onboarding/src/main/java/com/wanted/onboarding/filter/JwtProperties.java
+++ onboarding/src/main/java/com/wanted/onboarding/filter/JwtProperties.java
@@ -2,7 +2,7 @@ package com.wanted.onboarding.filter;
 
 public interface JwtProperties {
 	String SECRET = "songsong"; // 우리 서버만 알고 있는 비밀값
-	int EXPIRATION_TIME = 864000000; // 10일 (1/1000초)
+	int EXPIRATION_TIME = 36000000; // 10분
 	String TOKEN_PREFIX = "Bearer ";
 	String HEADER_STRING = "Authorization";
 }
